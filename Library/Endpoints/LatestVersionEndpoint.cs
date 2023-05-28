5138883dee7d059ad0b47a09b470bd3eaa737c3f
Mario Steele <mario@ruby-im.net>
2023-05-27 21:26:18 -0500

    Create LatestVersionEndpoint.cs

---
+++ Library/Endpoints/LatestVersionEndpoint.cs
@@ -0,0 +1,16 @@
+using TuxfamilyScraper.Library.SimpleAPI;
+
+namespace TuxfamilyScraper.Library.Endpoints;
+
+public class LatestVersionEndpoint : IEndpointDefinition
+{
+    public void DefineServices(IServiceCollection services)
+    {
+        
+    }
+
+    public void DefineEndpoints(WebApplication app)
+    {
+        
+    }
+}
